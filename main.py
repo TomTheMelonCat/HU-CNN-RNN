f3a983bdbba34a1bca53ec9c5b8486e8d5c02feb
Jiri Kadlec <jiri.kadlec@cgi.com>
2023-06-11 01:21:39 +0200

    Update: HU-CNN-RNN: Added dataframe text preprocessing (partially working)

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from package.utilities.statistics import Statistics
+from package.RNN.preprocessing import Preprocessing
 
 
 def main() -> None:
-    pass
+    preprocessing = Preprocessing("data\\News_Category_Dataset_v3.json")
+    stat_obj = Statistics(preprocessing)
+    stat_obj.display_dataset_statistics(save=True)
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     main()
--- package/utilities/data_io.py
+++ package/utilities/data_io.py
@@ -2,17 +2,18 @@ import pandas as pd
 import os
 from typing import Text
 
+
 class DataIO:
     def __init__(self) -> None:
         pass
-    
+
     @staticmethod
     def load_data(dataset_path: Text) -> None:
         try:
             return pd.read_json(dataset_path, lines=True)
         except Exception as ex:
             print(ex)
-    
+
     @staticmethod
     def save_statistics(statistics: Text = "") -> None:
         if not os.path.exists("statistics"):
